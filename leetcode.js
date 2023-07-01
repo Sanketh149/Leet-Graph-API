da816bd3b2c3b6d46084c98e86e6512db08e2843
sanketh149 <k.sanketh123@gmail.com>
2023-07-01 22:33:40 +0530

    Initial Commit

---
+++ leetcode.js
@@ -0,0 +1,43 @@
+const query = `
+  query getUserContestRankingHistory($username: String!) {
+    userContestRankingHistory(username: $username) {
+      attended
+          trendDirection
+          problemsSolved
+          totalProblems
+          finishTimeInSeconds
+          rating
+          ranking
+          contest
+          {
+            title
+            startTime
+          }
+    }
+  }
+`;
+
+//fetching the data
+exports.leetcode = (req, res) => {
+  let user = req.params.id;
+  fetch("https://leetcode.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Referer: "https://leetcode.com",
+    },
+    body: JSON.stringify({ query: query, variables: { username: user } }),
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      if (data.errors) {
+        res.send(data);
+      } else {
+        res.send(data.data);
+      }
+    })
+    .catch((err) => {
+      console.error("Error", err);
+      res.send(err);
+    });
+};
